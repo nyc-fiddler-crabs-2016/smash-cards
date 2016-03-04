c986dbf728fcadada13e434542a51b2d8ef149e4
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-03-04 12:21:59 -0500

    Add round validations.

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,7 @@ class Round < ActiveRecord::Base
   belongs_to :deck
 
   has_many :guesses
+
+  validates :first_try_count, presence: true, numericality: true
+  validates :total_guesses, presence: true, numericality: true
 end
